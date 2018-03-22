72d56b2b5a5f4b04e068588cfb4ff4f64ad40409
qrfaction <1909435492@qq.com>
2018-03-22 22:29:32 +0800

    更新2d切片和3d切片生成器

--- tool.py
+++ tool.py
@@ -1,11 +1,11 @@
 from nipy.io.api import save_image,load_image
 from nipy.core.api import Image, AffineTransform
-from setting import LABEL_PATH,IMAGE_PATH,INFO,OUTPUT,PRE_LABEL_PATH,INFO
+from setting import LABEL_PATH,IMAGE_PATH,INFO,OUTPUT,PRE_LABEL_PATH,INFO,crop_setting
 import torch
 from torch.utils.data import Dataset
 import numpy as np
 import json
-from model import dice_metric
+from random import randint,choice
 
 def get_files(path,prefix = True):
     import os
@@ -17,100 +17,192 @@ def get_files(path,prefix = True):
     return np.array(files)
 
 def get_image(f):
-    image = np.load(f).tolist()
-    return torch.FloatTensor(image)
+    return np.load(f)
 
 def get_batch_images(files):
-    batch_images = []
-    for f in files:
-        batch_images.append(np.load(f).tolist())
-    return torch.FloatTensor(batch_images)
+    return [get_image(f).tolist() for f in files]
 
-class Generator(Dataset):
+def crop_3d(image,id_h,area=None):
+    """
+                 x1        y1       z1            x2       y2      z2         x      y         z
+    180        115:161   118:172   55:81   90   115:162  112:174  99:121    37:214  36:239   28:149
+    192        75:120    81:135    46:70   78   74:122   80:134   86:114    15:160  20:182   16:143
+    256_166    100:163   105:173   48:75   82   98:162   107:173  90:117    25:215  28:237   19:144
+
+    80 * 80 * 40
+    :param image: 图像
+    :param id_h: 海马体ID
+    :param area:  crop区域
+    :return:
+    """
+    if area is None:
+        area = crop_setting['3d'+str(image.shape[2])]
+    z = 'z'+str(id_h)
+    return image[
+                area['x'][0]:area['x'][1],
+                area['y'][0]:area['y'][1],
+                area[z][0]:area[z][1]
+            ]
+
+def crop_2d_slice(image,id_h,axis,shift,window=5,area=None):
+    window = window//2
+    if area is None:
+        area = crop_setting['2d'+str(image.shape[2])]
+    z = 'z'+str(id_h)
+    if axis == 'x':
+        return image[
+            area['x'][0]+shift-window : area['x'][0]+shift+window+1,
+            area['y'][0]:area['y'][1],
+            area[z][0]:area[z][1]
+        ]
+    elif axis == 'y':
+        return image[
+            area['x'][0]:area['x'][1],
+            area['y'][0] + shift - window : area['y'][0] + shift + window + 1,
+            area[z][0]:area[z][1]
+        ]
+    elif axis == 'z':
+        return image[
+            area['x'][0]:area['x'][1],
+            area['y'][0]:area['y'][1],
+            area[z][0] + shift - window : area[z][0] + shift + window + 1,
+                ]
+    else:
+        raise ValueError('crop_2d axis error')
+
+class Generator_3d(Dataset):
     def __init__(self,files):
-        self.images = np.array([IMAGE_PATH + f for f in files])
-        self.labels = np.array([LABEL_PATH + f for f in files])
-        self.filename = files
-        with open(INFO + 'image_info.json', 'r') as f:
-            self.im_info = json.loads(f.read())
+        self.images = []
+        self.labels = []
+        for i in [1,2]:
+            for f in files:
+                self.images.append((IMAGE_PATH+f,i))
+                self.labels.append((LABEL_PATH+f,i))
 
-        self.length = len(files)
+        self.length = len(self.images)
 
     def __getitem__(self, index):  # 返回的是tensor
-        images = get_image(self.images[index])
-        labels = get_image(self.labels[index])
-        return images,labels
+        image_file,h_id = self.images[index]
+        label_file,h_id = self.labels[index]
+        image = get_image(image_file)
+        label = get_image(label_file)
+        data = np.concatenate([image,label],axis=-1)
+        assert data.shape == (80,80,40,2)
+        data = self.process(data,h_id)
+        image = data[:1]
+        label = data[1:]
+        return torch.FloatTensor(image),torch.FloatTensor(label)
+
+    def process(self,image,h_id):
+        image = crop_3d(image,h_id)
+        image = self.flip(image)
+        image = image.transpose((3,0,1,2))
+        return image
 
     def __len__(self):
         return self.length
-    
-    
-    def Gs_noise(self,noise_num=5):
-        images = images + torch.randn(images.shape) * num
-    
-    #dimensional=1,S-I
-    #dimensional=2,A-P
-    #dimensional=3,R-L
-    def randon_crop(self,dimensional=1):
-        random_num=np.random.randint(1,images.shape[dimensional]-2)
-        if dimensional==1:
-            return images[:,random_num-1:random_num+2,:,:]
-        if dimensional==2:
-            return images[:,:,random_num-1:random_num+2,:]
-        if dimensional==3:
-            return images[:,:,:,random_num-1:random_num+2]
 
-            
+    def flip(self,image):
+        axis = randint(0, 2)
+        if axis == 0:
+            image = image[::-1, :, :, :]
+        elif axis == 1:
+            image = image[:, ::-1, :, :]
+        elif axis == 2:
+            image = image[:, :, ::-1, :]
+        else:
+            raise ValueError('3d generator flip error')
+        return image
 
-def get_total_segment(y_pred,filename):
-    # print(y_pred)
-    # print(y_pred.shape)
+    def Gs_noise(self,image,noise_num=5):
+        image = image + torch.randn(image.shape) * noise_num
+
+class Generator_2d_slice(Dataset):
+    def __init__(self, files,axis):
+        self.axis = axis
+        if axis == 'x':
+            size = crop_setting['2d166']['x'][1] - crop_setting['2d166']['x'][0]
+        elif axis == 'y':
+            size = crop_setting['2d166']['y'][1] - crop_setting['2d166']['y'][0]
+        elif axis == 'z':
+            size = crop_setting['2d166']['z1'][1] - crop_setting['2d166']['z1'][0]
+        else:
+            raise ValueError('generator 2d slice axis error')
+        self.flip_axis = ['x','y','z']
+        self.flip_axis.remove(axis)
+
+        self.images = []
+        self.labels = []
+        for i in [1, 2]:
+            for shift in range(size):
+                for f in files:
+                    self.images.append((IMAGE_PATH+f,i,shift))
+                    self.labels.append((LABEL_PATH+f,i,shift))
+
+        self.length = len(self.images)
+
+    def __getitem__(self, index):  # 返回的是tensor
+        image_file,h_id,shift = self.images[index]
+        label_file,h_id,shift = self.labels[index]
+        image = get_image(image_file)
+        label = get_image(label_file)
+        data = np.concatenate([image, label], axis=-1)
+        data = self.process(data, h_id,shift)
+        image = data[0]
+        label = data[1]
+        return torch.FloatTensor(image),torch.FloatTensor(label)
+
+    def process(self, image, h_id,shift):
+        image = crop_2d_slice(image,h_id,self.axis,shift)
+        image = self.flip(image)
+        image = image.transpose((3, 0, 1, 2))
+        return image
+
+    def __len__(self):
+        return self.length
+
+    def flip(self,image):
+        axis = choice(self.flip_axis)
+        if axis == 'x':
+            image = image[::-1, :, :, :]
+        elif axis == 'y':
+            image = image[:, ::-1, :, :]
+        elif axis == 'z':
+            image = image[:, :, ::-1, :]
+        else:
+            raise ValueError('3d generator flip error')
+        return image
+
+
+
+def seg_recovery_3d(y_pred,filename):
     label = load_image(PRE_LABEL_PATH + filename)
-    # L = np.transpose(L,(3,0,1,2))
+    shape = label.get_data().shape
+    segment = np.zeros(shape)
 
-    # val_labels = LABEL_PATH + filename[:-7] + '.npy'
-    # ll = np.load(val_labels)
-    y_pred = y_pred.argmax(axis=0)
+    h1 = y_pred[0].around()
+    h2 = y_pred[1].around()
 
-    # x1 = np.sum(label[:, :, :] == 1)
-    # x2 = np.sum(label[:, :, :] == 2)
-    # print('label',x1+x2,x1,x2)
+    area = crop_setting['3d' + str(shape[2])]
 
-    # y_pred = y_pred.argmax(axis=0)     # (x,y,z)
-    #
-    # y_pred = np.expand_dims(y_pred,axis=3)  #(x,y,z,1)
-    # y1 = np.sum(y_pred[:, :, :] == 1)
-    # y2 = np.sum(y_pred[:, :, :] == 2)
-    # print('y_pred', y1 + y2,y1,y2)
+    segment[
+        area['x'][0]:area['x'][1],
+        area['y'][0]:area['y'][1],
+        area['z1'][0]:area['z1'][1],
+        0
+    ] = h1[0]
+    segment[
+        area['x'][0]:area['x'][1],
+        area['y'][0]:area['y'][1],
+        area['z2'][0]:area['z2'][1],
+        1
+    ] = h2[0]
 
-    # x1 = label>0
-    # x2 = y_pred>0
-
-
-    with open(INFO+'shape.json','r') as f:
-        shape_info = json.loads(f.read())
-    if shape_info[filename] == '160':
-        # print(np.sum(x1[60:140, 60:152, 30:130] * x2)/(x1.sum()+x2.sum()))
-        # print(np.sum(label[60:140, 60:152, 30:130,0]==ll))
-        segment = np.zeros((192,192,160,1))
-        segment[60:140, 60:152, 30:130,0] = y_pred
-    elif shape_info[filename] == '166':
-        # print(np.sum(x1[90:170, 95:187, 30:130] * x2) / (x1.sum() + x2.sum()))
-        # print(np.sum(label[90:170, 95:187, 30:130,0] == ll))
-        segment = np.zeros((256,256, 166, 1))
-        segment[90:170, 95:187, 30:130,0] = y_pred
-    elif shape_info[filename] == '180':
-        # print(np.sum(x1[100:180, 100:192, 40:140] * x2) / (x1.sum() + x2.sum()))
-        # print(np.sum(label[100:180, 100:192, 40:140,0] == ll))
-        segment = np.zeros((256,256,180, 1))
-        segment[100:180, 100:192, 40:140,0] = y_pred
-    else:
-        raise ValueError('shape error')
+    img = Image(segment,label.coordmap)
+    save_image(img,OUTPUT+filename)
 
 
-# =============================================================================
-# 把一个三维array的size转为(1,256,256,160)
-# =============================================================================
+
 
 def size_exchange(array):
     array1=[]
